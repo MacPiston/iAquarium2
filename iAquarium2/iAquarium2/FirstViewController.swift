60b48c404d0f985f87e792f49fb04330d4b9a6a4
MacPiston <mcpiston@gmail.com>
2020-04-09 19:13:12 +0200

    Generate project

---
+++ iAquarium2/iAquarium2/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  iAquarium2
+//
+//  Created by Maciej Zajecki on 09/04/2020.
+//  Copyright © 2020 Maciej Zajecki. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iAquarium2/iAquarium2/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  iAquarium2
+//
+//  Created by Maciej Zajecki on 09/04/2020.
+//  Copyright © 2020 Maciej Zajecki. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
